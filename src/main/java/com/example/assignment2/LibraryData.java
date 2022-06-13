4303625c4572e8e9faf43374621b08f07e62eb42
Eric Thomas <ericthomas@tuta.io>
2022-06-12 23:45:13 -0230

    Create LibraryData.java

---
+++ src/main/java/com/example/assignment2/LibraryData.java
@@ -0,0 +1,31 @@
+package com.example.assignment2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "libraryData", value = "/library-data")
+public class LibraryData extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "This is it!";
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    public void destroy() {
+    }
+}
