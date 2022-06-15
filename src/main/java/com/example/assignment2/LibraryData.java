53f4ce73946f9b060f265379749bcc6463f759c8
Eric Thomas <eric@ericthomas.ca>
2022-06-15 15:31:23 -0230

    june 15 update

--- src/main/java/com/example/assignment2/LibraryData.java
+++ src/main/java/com/example/assignment2/LibraryData.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet("/library-data")
@@ -21,13 +23,26 @@ public class LibraryData extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
         resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
 
-        //TODO Use a variable "view" to determine book or author query
+        out.println("<html><body>");
+        out.println("<h1> HEADER </h1>");
+        out.println("</body></html>");
 
-        List<Book> bookList = DBConnection.getAllBooks();
-        req.setAttribute("booklist", bookList);
-        req.getRequestDispatcher("viewallbooks.jsp").forward(req, resp);
+//      resp.setContentType("text/html");
+
+        // TODO Use a variable "view" to determine book or author query
+
+//        List<Book> bookList = null;
+//        try {
+//            bookList = DBConnection.getAllBooks();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        req.setAttribute("booklist", bookList);
+//        req.getRequestDispatcher("viewallbooks.jsp").forward(req, resp);
 
         //TODO add the list to the req
 
@@ -41,7 +56,17 @@ public class LibraryData extends HttpServlet {
         String title = req.getParameter("title");
         int editionNumber = Integer.parseInt(req.getParameter("edition_number"));
         String copyright = req.getParameter("copyright");
+        PrintWriter writer = resp.getWriter();
+
         Book newBook = new Book(isbn, title,editionNumber, copyright);
+
+        writer.print("<html><body>");
+        writer.print("<h1>Book Details</h1><br/>");
+        writer.print("ISBN: "+ newBook.getIsbn() + "<br/>");
+        writer.print("Title: "+ newBook.getTitle() +"<br/>");
+        writer.print("Edition Number: "+ newBook.getEditionNumber() +"<br/>");
+        writer.print("Copyright: "+ newBook.getCopyright());
+        writer.print("</body></html>");
 
         //TODO Here I want to handle the New Author and New Book form posts
 
