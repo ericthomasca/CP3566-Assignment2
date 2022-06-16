afcbf1c6756fc69efe85026df1fa53ed8b932efc
Eric Thomas <eric@ericthomas.ca>
2022-06-15 23:12:59 -0230

    complete doGet

--- src/main/java/com/example/assignment2/LibraryData.java
+++ src/main/java/com/example/assignment2/LibraryData.java
@@ -1,6 +1,5 @@
 package com.example.assignment2;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +14,6 @@ import java.util.Objects;
 @WebServlet("/library-data")
 public class LibraryData extends HttpServlet {
 
-
     public void init() {
     }
 
@@ -23,65 +21,63 @@ public class LibraryData extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
+        String dataType = req.getParameter("dataType");
 
-        //LOAD BOOKS
-        LinkedList<Book> bookList;
-        try {
-            bookList = DBConnection.getAllBooks();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        if (Objects.equals(dataType, "books")) {
+            LinkedList<Book> bookList;
+            try {
+                bookList = DBConnection.getAllBooks();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
-        //PRINT HEADERS
-        out.println("<h1>Books</h1><br />");
-        out.println("<table>");
-        out.println("<tr>");
-        out.println("<th>ISBN</th>");
-        out.println("<th>Title</th>");
-        out.println("<th>Edition</th>");
-        out.println("<th>Copyright</th>");
-        out.println("</tr>");
-
-        // PRINT BOOKS
-        for (Book book : bookList) {
+            out.println("<h1>Books</h1>");
+            out.println("<table>");
             out.println("<tr>");
-            out.println("<td>" + book.getIsbn() + "</td>");
-            out.println("<td>" + book.getTitle() + "</td>");
-            out.println("<td>" + book.getEditionNumber() + "</td>");
-            out.println("<td>" + book.getCopyright() + "</td>");
+            out.println("<th>ISBN</th>");
+            out.println("<th>Title</th>");
+            out.println("<th>Edition</th>");
+            out.println("<th>Copyright</th>");
             out.println("</tr>");
-        }
-        out.println("</table>");
-        out.println("<br /><br />");
 
-        // LOAD AUTHORS
-        LinkedList<Author> authorList;
-        try {
-            authorList = DBConnection.getAllAuthors();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+            for (Book book : bookList) {
+                out.println("<tr>");
+                out.println("<td>" + book.getIsbn() + "</td>");
+                out.println("<td>" + book.getTitle() + "</td>");
+                out.println("<td>" + book.getEditionNumber() + "</td>");
+                out.println("<td>" + book.getCopyright() + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</table>");
+            out.println("<br /><br />");
+        } else if (Objects.equals(dataType, "authors")) {
+            LinkedList<Author> authorList;
+            try {
+                authorList = DBConnection.getAllAuthors();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
-        // PRINT HEADERS
-        out.println("<h1>Authors</h1><br />");
-        out.println("<table>");
-        out.println("<tr>");
-        out.println("<th>ID</th>");
-        out.println("<th>First Name</th>");
-        out.println("<th>Last Name</th>");
-        out.println("</tr>");
-
-        // PRINT AUTHORS
-        for (Author author: authorList) {
+            out.println("<h1>Authors</h1>");
+            out.println("<table>");
             out.println("<tr>");
-            out.println("<td>" + author.getId() + "</td>");
-            out.println("<td>" + author.getFirstName() + "</td>");
-            out.println("<td>" + author.getLastName() + "</td>");
+            out.println("<th>ID</th>");
+            out.println("<th>First Name</th>");
+            out.println("<th>Last Name</th>");
             out.println("</tr>");
-        }
-        out.println("</table>");
-        out.println("<br /><br />");
 
+            for (Author author: authorList) {
+                out.println("<tr>");
+                out.println("<td>" + author.getId() + "</td>");
+                out.println("<td>" + author.getFirstName() + "</td>");
+                out.println("<td>" + author.getLastName() + "</td>");
+                out.println("</tr>");
+            }
+            out.println("</table>");
+        } else {
+            out.println("<h1>Data Type " + dataType + " Not Found</h1>");
+            out.println("<h3><a href=\"index.jsp\">Home</a></h3>");
+        }
     }
 
     @Override
@@ -103,8 +99,8 @@ public class LibraryData extends HttpServlet {
             }
 
             out.print("<html><body>");
-            out.print("<h1>Book Submitted!!</h1><br/>");
-            out.print("<h2>Book Details</h2><br/>");
+            out.print("<h1>Book Submitted!!</h1>");
+            out.print("<h2>Book Details</h2>");
             out.print("ISBN: " + isbn + "<br/>");
             out.print("Title: " + title + "<br/>");
             out.print("Edition Number: " + editionNumber + "<br/>");
@@ -123,8 +119,8 @@ public class LibraryData extends HttpServlet {
             }
 
             out.print("<html><body>");
-            out.print("<h3>Author Submitted!!</h3><br/>");
-            out.print("<h2>Author Details</h2><br/>");
+            out.print("<h1>Author Submitted!!</h1>");
+            out.print("<h2>Author Details</h2>");
             out.print("ID: " + id + "<br/>");
             out.print("First Name: " + firstName + "<br/>");
             out.print("Last Name: " + lastName);
